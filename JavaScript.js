608a2a9d873e28250d78838c4956375cfd922af7
Fabian Gal <fabiangal.45@gmail.com>
2020-12-18 19:14:47 +0000

    working on x's

--- JavaScript.js
+++ JavaScript.js
@@ -1,24 +1,41 @@
-//this will just run  
-//and display where the script tag is placed in the html file 
-var player = X; // player 
-var computer = 0; // computer
+// Fabian Gal
 
-PlayebleSlots=( r1c1,r1c2,r1c3,r2c1,r2c2,r2c3,r3c1,r3c2,r3c3); 
-// option to be playeble by both player and computer
-playerChoice=();
-computerChoice();
+function myFunction() {
+	var showOnClick = document.getElementById("r1c1");
+	showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+}
 
-winningPlaysArea=(     
-v1 = {r1c1, r1c2, r1c3}
-v2 = {r2c1, r2c2, r2c3}
-v3 = {r3c1, r3c2, r3c3}
-h1 = {r1c1, r2c1, r3c1}
-h2 = {r1c2, r2c2, r3c2}
-h3 = {r1c3, r2c3, r3c3}
-x1 = {r1c1, r2c2, r3c3}
-x2 = {R1c3, r2c2, r3c1});
-// winning condiction
 
-if(computer has 2 || 3 slots of winning){
-	display = "play the missing slot";
 
+
+
+
+
+
+
+
+// // Emiraldo Fernandes
+// //this will just run  
+// //and display where the script tag is placed in the html file 
+// var player = X; // player 
+// var computer = 0; // computer
+
+// PlayebleSlots=( r1c1,r1c2,r1c3,r2c1,r2c2,r2c3,r3c1,r3c2,r3c3); 
+// // option to be playeble by both player and computer
+// playerChoice=();
+// computerChoice();
+
+// winningPlaysArea=(     
+// v1 = {r1c1, r1c2, r1c3}
+// v2 = {r2c1, r2c2, r2c3}
+// v3 = {r3c1, r3c2, r3c3}
+// h1 = {r1c1, r2c1, r3c1}
+// h2 = {r1c2, r2c2, r3c2}
+// h3 = {r1c3, r2c3, r3c3}
+// x1 = {r1c1, r2c2, r3c3}
+// x2 = {R1c3, r2c2, r3c1});
+// // winning condiction
+
+// if(computer has 2 || 3 slots of winning){
+// 	display = "play the missing slot";
+// }
