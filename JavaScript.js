01ee0fda694c6c64b9828b2775a9b852029c76fc
Fabian Gal <fabiangal.45@gmail.com>
2020-12-18 21:52:34 +0000

    added function for each x and o

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,99 @@
 // Fabian Gal
 
-function myFunction() {
-	
-	
+
+// This is the boxes that are going to display the x on click. There must have been a better way to do it unfortunatelly I am not aware of it for the time being
+function oneX() {
+	var showOnClick = document.getElementById("1"); // changing the number will change the position that the x will apear in but you can click anywhere with the specific funtion
+    showOnClick.querySelector("img.x").classList.remove("game-hide-x");
 }
+function twoX() {
+	var showOnClick = document.getElementById("2");
+    showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+}
+function threeX() {
+	var showOnClick = document.getElementById("3");
+    showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+}
+
+function fourX() {
+	var showOnClick = document.getElementById("4"); 
+    showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+}
+function fiveX() {
+	var showOnClick = document.getElementById("5");
+    showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+}
+function sixX() {
+	var showOnClick = document.getElementById("6");
+    showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+}
+
+function sevenX() {
+	var showOnClick = document.getElementById("7"); 
+    showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+}
+function eightX() {
+	var showOnClick = document.getElementById("8");
+    showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+}
+function nineX() {
+	var showOnClick = document.getElementById("9");
+    showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+}
+
+
+
+// for the O
+
+function oneO() {
+	var showOnClick = document.getElementById("1");
+    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+}
+function twoO() {
+	var showOnClick = document.getElementById("2");
+    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+}
+function threeO() {
+	var showOnClick = document.getElementById("3");
+    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+}
+function fourO() {
+	var showOnClick = document.getElementById("4");
+    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+}
+function fiveO() {
+	var showOnClick = document.getElementById("5");
+    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+}
+function sixO() {
+	var showOnClick = document.getElementById("6");
+    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+}
+function sevenO() {
+	var showOnClick = document.getElementById("7");
+    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+}
+function eightO() {
+	var showOnClick = document.getElementById("8");
+    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+}
+function nineO() {
+	var showOnClick = document.getElementById("9");
+    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -20,13 +110,13 @@ function myFunction() {
 // var player = X; // player 
 // var computer = 0; // computer
 
-// PlayebleSlots=( r1c1,r1c2,r1c3,r2c1,r2c2,r2c3,r3c1,r3c2,r3c3); 
+// PlayebleSlots=( one,two,three,four,five,six,r3c1,r3c2,r3c3); 
 // // option to be playeble by both player and computer
 // playerChoice=();
 // computerChoice();
 
 // winningPlaysArea=(     
-// v1 = {r1c1, r1c2, r1c3}
+// v1 = {one, two, three}
 // v2 = {r2c1, r2c2, r2c3}
 // v3 = {r3c1, r3c2, r3c3}
 // h1 = {r1c1, r2c1, r3c1}
