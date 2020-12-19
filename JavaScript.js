3bcff7d7a96f6c3677b00a2d367b65f704bc7224
Fabian Gal <fabiangal.45@gmail.com>
2020-12-19 00:18:00 +0000

    reworking on the code

--- JavaScript.js
+++ JavaScript.js
@@ -2,84 +2,77 @@
 
 
 // This is the boxes that are going to display the x on click. There must have been a better way to do it unfortunatelly I am not aware of it for the time being
+// I am keeping it like this for now as it works and if there is time I will hopefully look on how to rewrite this.
+
+
+
+var playerChoice;
+
 function oneX() {
 	var showOnClick = document.getElementById("1"); // changing the number will change the position that the x will apear in but you can click anywhere with the specific funtion
     showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+    console.log("you have picked slot no: 1");
+    playerChoice= 0;
+    console.log(playerChoice);
 }
+console.log(playerChoice);
 function twoX() {
 	var showOnClick = document.getElementById("2");
     showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+    console.log("you have picked slot no: 2");
+    playerChoice= 1;
+    console.log(playerChoice);
 }
 function threeX() {
 	var showOnClick = document.getElementById("3");
     showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+    console.log("you have picked slot no: 3");
+    playerChoice= 2;
+    console.log(playerChoice);
 }
 
 function fourX() {
 	var showOnClick = document.getElementById("4"); 
     showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+    console.log("you have picked slot no: 4");
+    playerChoice= 3;
+    console.log(playerChoice);
 }
 function fiveX() {
 	var showOnClick = document.getElementById("5");
     showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+    console.log("you have picked slot no: 5");
+    playerChoice= 4;
+    console.log(playerChoice);
 }
 function sixX() {
 	var showOnClick = document.getElementById("6");
     showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+    console.log("you have picked slot no: 6");
+    playerChoice= 5;
+    console.log(playerChoice);
 }
 
 function sevenX() {
 	var showOnClick = document.getElementById("7"); 
     showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+    console.log("you have picked slot no: 7");
+    playerChoice= 6;
+    console.log(playerChoice);
 }
 function eightX() {
 	var showOnClick = document.getElementById("8");
     showOnClick.querySelector("img.x").classList.remove("game-hide-x");
+    console.log("you have picked slot no: 8");
+    playerChoice= 7;
+    console.log(playerChoice);
 }
 function nineX() {
 	var showOnClick = document.getElementById("9");
     showOnClick.querySelector("img.x").classList.remove("game-hide-x");
-}
-
-
-
-// for the O
-
-function oneO() {
-	var showOnClick = document.getElementById("1");
-    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
-}
-function twoO() {
-	var showOnClick = document.getElementById("2");
-    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
-}
-function threeO() {
-	var showOnClick = document.getElementById("3");
-    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
-}
-function fourO() {
-	var showOnClick = document.getElementById("4");
-    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
-}
-function fiveO() {
-	var showOnClick = document.getElementById("5");
-    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
-}
-function sixO() {
-	var showOnClick = document.getElementById("6");
-    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
-}
-function sevenO() {
-	var showOnClick = document.getElementById("7");
-    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
-}
-function eightO() {
-	var showOnClick = document.getElementById("8");
-    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
-}
-function nineO() {
-	var showOnClick = document.getElementById("9");
-    showOnClick.querySelector("img.o").classList.remove("game-hide-o");
+    console.log("you have picked slot no: 9");
+    playerChoice= 8;
+    console.log(playerChoice);
 }
 
 
@@ -91,41 +84,3 @@ function nineO() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Emiraldo Fernandes
-// //this will just run  
-// //and display where the script tag is placed in the html file 
-// var player = X; // player 
-// var computer = 0; // computer
-
-// PlayebleSlots=( one,two,three,four,five,six,r3c1,r3c2,r3c3); 
-// // option to be playeble by both player and computer
-// playerChoice=();
-// computerChoice();
-
-// winningPlaysArea=(     
-// v1 = {one, two, three}
-// v2 = {r2c1, r2c2, r2c3}
-// v3 = {r3c1, r3c2, r3c3}
-// h1 = {r1c1, r2c1, r3c1}
-// h2 = {r1c2, r2c2, r3c2}
-// h3 = {r1c3, r2c3, r3c3}
-// x1 = {r1c1, r2c2, r3c3}
-// x2 = {R1c3, r2c2, r3c1});
-// // winning condiction
-
-// if(computer has 2 || 3 slots of winning){
-// 	display = "play the missing slot";
-// }
